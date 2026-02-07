3dd10ac51107f58e4cefcb901c9dace2dd132e1a
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-02-07 21:55:26 +0100

    ChevronDown button inserted into Hero for next section navigation

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -111,6 +111,14 @@ const Hero = () => {
                     </FadeIn>
                 </div>
             </div>
+
+            {/* Scroll Indicator */}
+            <button
+                onClick={() => scrollToSection('about')}
+                className='absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce'
+            >
+                <ChevronDown className='w-8 h-8 text-primary'/>
+            </button>
         </section>
     );
 };
